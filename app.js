0582c03f80de97b0155ae69dde60e6317f61eb9d
Victor Dupré <dupre.victor@live.fr>
2023-11-15 16:17:39 +0100

    exo js

--- app.js
+++ app.js
@@ -13,3 +13,28 @@ fruits.forEach((fruit) => {
     </div>
   </article>`;
 });
+
+/* on reçoit en paramètre l'évenement click */
+const handleClick = (event) => {
+  console.log(event);
+  /* event.currentTarget est l'élément HTML qui a perçu le clic 
+  (notre <article></article>) */
+  console.log(event.currentTarget);
+  /* Sur cet article, on vient sélectionner le <h2></h2> */
+  const title = event.currentTarget.querySelector("h2");
+  console.log(title);
+  //title.style.color = "red";
+  /*On affiche le contenu HTML de notre <h2></h2>,
+   soit le titre de la recette */
+  alert(title.innerHTML);
+};
+
+/* On récupère tous les <article></article> de la page sous forme de tableau */
+const articles = document.querySelectorAll("article");
+console.log(articles);
+
+/* Pour chaque <article></article> dans notre tableau,
+on vient gérer l'évenement "click" avec la fonction handleClick */
+articles.forEach((article) => {
+  article.onclick = handleClick;
+});
