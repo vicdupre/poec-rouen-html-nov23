0d80dc9cdf6fbf3220d48df74987f0173f0f6e02
Victor Dupré <dupre.victor@live.fr>
2023-11-15 16:48:20 +0100

    modules js + promise

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import multiply from "./multiply.js";
+import { divide } from "./operations.js";
+
 const fruits = ["Fraise", "Citron", "Ananas", "Pêche"];
 
 const container = document.querySelector(".container");
@@ -22,7 +25,12 @@ const handleClick = (event) => {
   console.log(event.currentTarget);
   /* Sur cet article, on vient sélectionner le <h2></h2> */
   const title = event.currentTarget.querySelector("h2");
-  console.log(title);
+  try {
+    console.log(title);
+  } catch (error) {
+    console.error(error.name, error.message);
+  }
+
   //title.style.color = "red";
   /*On affiche le contenu HTML de notre <h2></h2>,
    soit le titre de la recette */
@@ -38,3 +46,25 @@ on vient gérer l'évenement "click" avec la fonction handleClick */
 articles.forEach((article) => {
   article.onclick = handleClick;
 });
+
+new Promise((resolve, reject) => {
+  try {
+    setTimeout(() => {
+      //tout s'est bien passé, on renvoie la donnée dans resolve ->bloc then
+      resolve("Terminé");
+    }, 1000);
+  } catch (error) {
+    //On a eu un problème, on renvoie l'erreur dans reject -> bloc catch
+    reject(error);
+  }
+})
+  //en cas de succès
+  .then((value) => {
+    console.log(value);
+  })
+  //en cas d'erreur
+  .catch((err) => console.log(err));
+
+console.log(multiply(2, 5));
+//console.log(add(2, 5));
+console.log(divide(2, 5));
---
+++ operations.js
@@ -0,0 +1,4 @@
+const add = (a, b) => a + b;
+const divide = (a, b) => a / b;
+export const sub = (a, b) => a - b;
+export { add, divide };
