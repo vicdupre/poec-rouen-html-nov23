99273a10aa635b0d28e9c56e8d9b959de5e63e1e
Victor Dupré <dupre.victor@live.fr>
2023-11-16 14:04:04 +0100

    local & session storage

--- form.js
+++ form.js
@@ -1,5 +1,10 @@
 const form = document.querySelector("form");
 
+const username = document.querySelector("p");
+
+const _name = sessionStorage.getItem("user");
+username.innerHTML = _name;
+
 form.onsubmit = function (event) {
   //On bloque le comportement naturel du navigateur
   event.preventDefault();
@@ -12,6 +17,18 @@ form.onsubmit = function (event) {
   // output as an object
   console.log(data);
 
+  username.innerHTML = data.email;
+
+  //le sessionStorage sera vidée à la fermeture du navigateur
+  sessionStorage.setItem("user", data.email);
+
   //On enregistre dans le stockage du navigateur nos informations sous forme de chaine de caractères
+  //Le localstorage ne sera pas vidé automatiquement
   localStorage.setItem("user", JSON.stringify(data));
+};
+
+const resetBtn = document.querySelector("button.btn-danger");
+resetBtn.onclick = () => {
+  sessionStorage.removeItem("user");
+  localStorage.clear();
 };
